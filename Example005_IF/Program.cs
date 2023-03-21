e214e6735e4afee2ba9d146e12e8eba445c1e538
Evgeniy Novichenko <tnrkip@yandex.ru>
2023-03-21 22:10:37 +0300

    add IF

---
+++ Example005_IF/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Введите имя пользователя");
+string userName = Console.ReadLine();
+
+if (userName.ToLower()=="маша"){
+    Console.WriteLine("Ура, это же Маша");
+}
+else{
+
+Console.WriteLine("Hello, "+userName);
+}
