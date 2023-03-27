2cb374df0039075f8464c67aced499299cc93e20
Evgeniy Novichenko <tnrkip@yandex.ru>
2023-03-27 21:10:41 +0300

    add seminar 2

---
+++ seminar_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// int num = new Random().Next(10,100);
+// int num2 = new Random().Next(10,100);
+// if(num>num2) {
+//     Console.WriteLine($" bolishe  {num}");
+// }else if (num<num2){
+//     Console.WriteLine($" bolishe  {num2}");
+// }else{
+//         Console.WriteLine($" ravno ");
+// }
+
+// int num = new Random().Next(100,1000);
+
+// char[] out1 = (Convert.ToString(num)).ToCharArray();
+// System.Console.WriteLine(out1);
+
+// задача3
+
+// Console.WriteLine("Vvedite chislo 1");
+// int input1 =int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Vvedite chislo 2");
+// int input2 =int.Parse(Console.ReadLine());
+// int num3=input1%input2;
+// if (num3==0){
+//     Console.WriteLine($"input1: {input1} кратно input2: {input2}");
+// }else{
+//     Console.WriteLine($"input2: {input2} не кратно input1: {input1} остаток {num3}");
+// }
+
+// задача4
