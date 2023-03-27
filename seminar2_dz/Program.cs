19b99b656899536c754e1d35ccd22a0f79446b5b
Evgeniy Novichenko <tnrkip@yandex.ru>
2023-03-27 22:14:32 +0300

    add dz seminar2

---
+++ seminar2_dz/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Console.WriteLine("Vvedite chislo 1");
+// int input1 =int.Parse(Console.ReadLine());
+
+// Console.WriteLine($"{input1/10%10}");
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+// Console.WriteLine("Vvedite chislo 1");
+// int input1 =int.Parse(Console.ReadLine());
+
+// if (input1<100){
+//     Console.WriteLine("3 chifri net");
+// }else{
+//     while (input1>1000){
+//         input1/=10;
+//     }
+//     Console.WriteLine($"{input1%10}");
+// }
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Console.WriteLine("Vvedite chislo 1");
+// int input1 =int.Parse(Console.ReadLine());
+
+// if (input1%6==0 ||input1%7==0){
+//     Console.WriteLine($"{input1} eto vihodnoi");
+// }else{
+//      Console.WriteLine($"{input1} eto ne vihodnoi");
+// }
--- seminar_2/Program.cs
+++ seminar_2/Program.cs
@@ -24,7 +24,34 @@
 // if (num3==0){
 //     Console.WriteLine($"input1: {input1} кратно input2: {input2}");
 // }else{
-//     Console.WriteLine($"input2: {input2} не кратно input1: {input1} остаток {num3}");
+//     Console.WriteLine($"input1: {input1} не кратно input2: {input2} остаток {num3}");
 // }
 
-// задача4
+// задача14
+// Console.WriteLine("Vvedite chislo");
+// int input1 =int.Parse(Console.ReadLine());
+
+
+// if ((input1%7==0 ) && (input1%23==0)  ){
+//     Console.WriteLine($"input1: {input1} кратно 7 и 23 ");
+// }else{
+//     Console.WriteLine($"input1: {input1} не кратно 7 и 23");
+// }
+
+// задача16
+
+// Console.WriteLine("Vvedite chislo 1");
+// int input1 =int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Vvedite chislo 2");
+// int input2 =int.Parse(Console.ReadLine());
+
+
+// if ((Math. Sqrt(input1)== input2) ||(Math. Sqrt(input2)== input1)){
+//     Console.WriteLine("da");
+// }else{
+//     Console.WriteLine("net");
+// }
+
+
+  
