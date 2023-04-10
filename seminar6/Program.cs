07de77a09fc156ee04b82d79be5993b7be452abd
Evgeniy Novichenko <tnrkip@yandex.ru>
2023-04-10 23:08:46 +0300

    add dz6

---
+++ seminar6/Program.cs
@@ -0,0 +1,89 @@
+﻿// int[] GetArray(int size)
+// {
+//     int[] result = new int[size];
+//     Random rnd = new Random();
+//     for (int i = 0; i < size; i++)
+//         result[i] = rnd.Next(0, 10);
+
+//     return result;
+// }
+
+// int[]arr=GetArray(13);
+// Console.WriteLine($"{String.Join(",",arr)}");
+// Array.Reverse(arr);
+// Console.WriteLine($"{String.Join(",",arr)}");
+
+// int[] MyRevers(int[] array)
+// {
+//     int [] result = new int[array.Length];
+
+//     for (int i = 0; i < array.Length; i++)
+//         result[i]=array[array.Length-1-i];
+
+//     return result;
+// }
+// Console.WriteLine($"{String.Join(",",MyRevers(arr))}");
+
+// void MyRever1(int[] array)
+// {
+
+//     for (int i = 0; i < array.Length/2; i++){
+//         int temp=array[i];
+//         array[i]=array[array.Length-1-i];
+//         array[array.Length-1-i]=temp;
+//     }
+
+
+// }
+// MyRever1(arr);
+// Console.WriteLine($"{String.Join(",",arr)}");
+
+// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+// bool Treug(int input1,int input2,int input3)
+// {
+
+
+//     if ((input1 + input2) > input3 && (input1 + input3) > input2 && (input3 + input2) > input1)
+//     {
+//                 return true;
+//     }
+//     else
+//     {
+//         return false;
+
+//     }
+// }
+// Console.Write("Vvedite chislo 1 ");
+// int input1 = int.Parse(Console.ReadLine());
+
+// Console.Write("Vvedite chislo 2 ");
+// int input2 = int.Parse(Console.ReadLine());
+
+// Console.Write("Vvedite chislo 3 ");
+// int input3 = int.Parse(Console.ReadLine());
+
+// Console.WriteLine(Treug(input1,input2,input3) ? "не является": "не является");
+
+// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+// Console.Write("Vvedite chislo 1 ");
+// int input1 = int.Parse(Console.ReadLine());
+// Console.Write("sis ");
+// int sis = int.Parse(Console.ReadLine());
+
+// string DecToBin(int dec, int sis)
+//     {
+//         var otv = string.Empty;
+//         while (dec > 0)
+//         {
+//             otv = (dec % sis) + otv;
+//             dec /= sis;
+//         }
+//         return otv;
+//     }
+
+// Console.WriteLine(DecToBin(input1,sis));
---
+++ seminar6_dz/Program.cs
@@ -0,0 +1,51 @@
+﻿class Program
+{
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+    static void Mchisla(){
+            Console.Write("Vvedite chislo chres probel ");
+            string input= Console.ReadLine();
+            string[] arr = input.Split(' ');
+            int result = 0;
+            foreach (var item in arr)
+            {
+                if (Convert.ToInt32(item)>0){
+                    result++;
+                }
+
+            }
+            Console.WriteLine(result);
+        }
+
+// // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+    static void urovnenie(){
+        Console.Write("Vvedite chislo chres probel ");
+
+        string [] input= Console.ReadLine().Split(' ');
+        
+        double b1 = Convert.ToInt32(input[0]);
+        double k1 = Convert.ToInt32(input[1]);
+        double b2 = Convert.ToInt32(input[2]);
+        double k2 = Convert.ToInt32(input[3]);
+
+        double x = (-b2 + b1)/(-k1 + k2);
+        double y = k2 * x + b2;
+
+        Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+        }
+    static void Main(string[] args)
+    {
+        Mchisla();
+        urovnenie();
+
+
+        
+    }
+}
+
+
+
+// 1, -7, 567, 89, 223-> 3
+
